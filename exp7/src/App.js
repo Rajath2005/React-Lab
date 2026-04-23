93816c0bdf5f629707d0f18377d1727d86f15eda
Your Name <your-email@example.com>
2026-04-23 23:24:18 +0530

    Updated exp8

--- exp7/src/App.js
+++ exp7/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ProfileCard from './ProfileCard';
+import React from "react";
+import ProfileCard from "./ProfileCard";
+
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>User Profile</h1>
+    <div>
+      <h1 style={{ textAlign: "center" }}>User Profile</h1>
+
       <ProfileCard
         name="John Doe"
         bio="A passionate developer with a love for learning and coding."
-        profilePicture="https://upload.wikimedia.org/wikipedia/commons/4/45/John_Healey_Official_Cabinet_Portrait%2C_July_2024_%28cropped%29_2.jpg"
+        profilePicture="https://img.freepik.com/free-photo/closeup-shot-beautiful-butterfly-with-interesting-textures-orange-petaled-flower_181624-7640.jpg?semt=ais_hybrid&w=740&q=80"
         backgroundColor="pink"
       />
     </div>
   );
 }
+
 export default App;
